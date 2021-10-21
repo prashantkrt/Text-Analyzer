59203c0e4ea37205ba61653de903949aeb2f0460
Prashant1 <prashantcool016@gmial.com>
2021-10-21 15:14:55 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import Form from "./components/Form";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Navbar title1="Home" title2="About Us" title3="Contact Us" />
+
       <div className="container my-3">
         <Form heading="Text Analyzer" />
       </div>
+
+      {/* <div className="container my-5">
+        <About title1="Developer " title2="About Page" />
+      </div> */}
     </>
   );
 }
